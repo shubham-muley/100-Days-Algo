9a408c1f938b6d6d7b71de2a736a6e8ca9642f7c
shubham-muley <shubhammuley7@gmail.com>
2021-03-24 00:13:45 +0530

    Test RepeatedChar Class

---
+++ Day11/Main_RC.java
@@ -0,0 +1,11 @@
+public class Main_RC {
+
+    public static void main(String[] args){
+
+        RepeatedChar finder = new RepeatedChar();
+        System.out.println(finder.findFirstNonRepeating("the string"));
+
+        System.out.println(finder.findFirstNonRepeating("aaaax"));
+
+    }
+}
