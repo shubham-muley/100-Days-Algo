a8bc33c71b43784e5fd6924574d78a25b88cb1be
shubham-muley <shubhammuley7@gmail.com>
2021-03-27 01:51:29 +0530

    Main class of RepeatedChar

---
+++ Day14/Main_RC.java
@@ -0,0 +1,11 @@
+public class Main_RC {
+
+    public static void main(String[] args){
+
+        RepeatedChar finder2 = new RepeatedChar();
+        System.out.println("The first Repeating character of apple is "+finder2.findfirstRepeatedChar_Map("apple"));
+
+        System.out.println("The first Repeating character of ammazing is "+finder2.findfirstRepeatedChar_Map("ammazing"));
+
+    }
+}
