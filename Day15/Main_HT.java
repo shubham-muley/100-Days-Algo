a592d20353516b785da2fa97d3f2131606f3f3c5
shubham-muley <shubhammuley7@gmail.com>
2021-03-29 03:01:59 +0530

    Hash Table

---
+++ Day15/Main_HT.java
@@ -0,0 +1,20 @@
+public class Main_HT {
+    public static void main(String[] args) {
+        HashTable h = new HashTable();
+
+        h.put(1,"Apple");
+        /*h.put(2,"Banana");
+        h.put(4,"Orange");
+        h.put(6,"Lime");
+        h.put(7,"Mango");
+
+        System.out.println(h.get(4));
+
+        System.out.println(h);
+
+        h.remove(4);
+        System.out.println(h);
+
+         */
+    }
+}
