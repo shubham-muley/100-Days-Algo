023f52df7e6346fc399aef9421996a096c433e9e
shubham-muley <79686018+shubham-muley@users.noreply.github.com>
2021-04-03 23:42:18 +0530

    Problem From HackerRank done using Counter

---
+++ Day20/SalesByMatch.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    count=0
+    c=Counter(ar)
+    for i in c.keys():
+        if(c[i]/2>=1):
+            count+=c[i]//2
+
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
