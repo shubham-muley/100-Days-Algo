07c9e5969cbdc97b8101b7785e6280dad084c14f
shubham-muley <79686018+shubham-muley@users.noreply.github.com>
2021-04-04 01:21:57 +0530

    Counting valleys problem from sequence of steps

---
+++ Day20/Counting_Valleys.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    val=0
+    alt=0
+    
+    for i in range(steps):
+        if(path[i]=='U'):
+            alt+=1
+        else:
+            alt-=1
+            
+        if(alt==0 and path[i]=='U'):
+            val+=1
+        
+    return val
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
